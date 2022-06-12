7af6f750ddceaf3e50defdcfa43f3548b828e5a0
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-06-12 21:38:07 +0200

    Added error documentation

--- error.go
+++ error.go
@@ -2,5 +2,9 @@ package collections
 
 import "fmt"
 
+// ErrIndexOutOfBounds indicates that the specified list index is too large for the amount of list elements, or an
+// iterator ran out of elements.
 var ErrIndexOutOfBounds = fmt.Errorf("index out of bounds")
+
+// ErrElementNotFound indicates that an IndexOf or similar operation did not find the specified element.
 var ErrElementNotFound = fmt.Errorf("element not found")
