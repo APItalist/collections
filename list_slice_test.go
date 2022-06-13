f8292269c7549830c47c9f5d1023ecd81c675a91
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-06-13 22:16:03 +0200

    More readable examples

--- list_slice_test.go
+++ list_slice_test.go
@@ -1,20 +1,25 @@
 package collections_test
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/apitalist/collections"
+	"github.com/apitalist/collections"
 )
 
 func ExampleSliceList_Add() {
-    // Create a new list
-    list := collections.NewSliceList("a", "b", "c", "d")
+	// Create a new list
+	list := collections.NewSliceList("a", "b", "c", "d")
 
-    // Add an element to the list
-    list.Add("e")
+	// Add an element to the list
+	list.Add("e")
 
-    // Iterate over the list. We ignore the returning error since our output function never fails.
-    _ = list.Iterator().ForEachRemaining(func(e string) error { fmt.Print(e); return nil })
+	// Iterate over the list. We ignore the returning error since our output function never fails.
+	_ = list.Iterator().ForEachRemaining(
+		func(e string) error {
+			fmt.Print(e)
+			return nil
+		},
+	)
 
-    // Output: abcde
+	// Output: abcde
 }
