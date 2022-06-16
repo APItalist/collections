3bc8c622baec630d8ceee4345e4e5097a1cbaf45
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-06-17 00:04:14 +0200

    Added list example

--- list_test.go
+++ list_test.go
@@ -7,6 +7,32 @@ import (
 	"github.com/apitalist/collections/slice"
 )
 
+func ExampleList() {
+	// The slice subpackage offers a go slice-backed list implementation
+	list := slice.New("a", "b", "c")
+
+	// Items can be added to lists:
+	list.Add("d")
+
+	// Or items can be removed:
+	list.Remove("a")
+
+	// We can also print all items in a list:
+	iterator := list.Iterator()
+	for iterator.HasNext() {
+		element, err := iterator.Next()
+		if err != nil {
+			// This should never happen:
+			panic(err)
+		}
+		fmt.Println(element)
+	}
+
+	// Output: b
+	// c
+	// d
+}
+
 func ExampleImmutableList() {
 	// The immutableslice subpackage offers a go slice-backed list implementation that is immutable
 	list := immutableslice.New("a", "b", "c")
